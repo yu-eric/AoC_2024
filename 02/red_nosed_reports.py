42cc39f48783acf622f4604804b108f8c3c22e5d
yu-eric <ejyu99@gmail.com>
2024-12-02 22:41:32 +0000

    Finish day 2

--- 02/red_nosed_reports.py
+++ 02/red_nosed_reports.py
@@ -6,10 +6,17 @@ list1 = []
 with open("2_input.txt", "r") as file:
     list1 = [list(map(int, line.split())) for line in file]
 
-num_safe = 0
-for i in list1:
+def is_safe(i):
     if i == sorted(i) or i == sorted(i, reverse=True):
         if 1 <= min(abs(x[0] - x[1]) for x in zip(i[1:], i)) <= max(abs(x[0] - x[1]) for x in zip(i[1:], i)) <= 3:
-            num_safe += 1
+            return True
+    return False
 
-print(num_safe)
+def is_tolerated(j):
+    for i in range(len(j)):
+        if is_safe(j[:i] + j[i+1:]):
+            return True
+    return False
+
+print(sum(is_safe(l) for l in list1))
+print(sum(is_safe(l) or is_tolerated(l) for l in list1))
