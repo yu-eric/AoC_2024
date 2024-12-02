f9cd84be1e4a0575c6b3236fb57d413659a486e6
yu-eric <ejyu99@gmail.com>
2024-12-02 19:09:24 +0000

    Finish day 1

--- 01/historian_hysteria.py
+++ 01/historian_hysteria.py
@@ -10,4 +10,7 @@ with open("1_input.txt", "r") as file:
         list1.append(x)
         list2.append(y)
 
-print(reduce(add,map(abs, map(sub, sorted(list1), sorted(list2)))))
+print(reduce(add, map(abs, map(sub, sorted(list1), sorted(list2)))))
+
+# part 2
+print(reduce(add, map(lambda x: x * list2.count(x), list1)))
