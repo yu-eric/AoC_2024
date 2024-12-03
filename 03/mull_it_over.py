88d1be65857d09b725d7cee22501a39be9f59f56
Eric Yu <ejyu99@gmail.com>
2024-12-03 20:56:07 +0000

    Finish day 3

--- 03/mull_it_over.py
+++ 03/mull_it_over.py
@@ -2,17 +2,19 @@ import re
 from operator import add, mul
 from functools import reduce
 
-# Define the pattern
-pattern = r"mul\((\d+),(\d+)\)"
+def mul_tuples(tuplist):
+    return reduce(add, map((lambda x: mul(int(x[0]), int(x[1]))), tuplist))
 
+text = ""
 with open("3_input.txt", "r") as file:
     text = file.read()
 
+clean_text = "".join([segment[segment.find("do()") :] for segment in ("do()" + text).split("don't()")])
 # Find all matches
+pattern = r"mul\((\d+),(\d+)\)"
 matches = re.findall(pattern, text)
+only_do = re.findall(pattern, clean_text)
 
-# Convert matches to tuples of integers
-result = [(int(x), int(y)) for x, y in matches]
-
-print(reduce(add, map((lambda x: mul(int(x[0]), int(x[1]))), matches)))
+print(mul_tuples(matches))
+print(mul_tuples(only_do))
 
