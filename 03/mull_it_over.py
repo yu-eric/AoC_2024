7140d00d3f46e862e94aa6055e8530936a169e10
Eric Yu <ejyu99@gmail.com>
2024-12-03 15:41:57 +0000

    Day 3 first star

---
+++ 03/mull_it_over.py
@@ -0,0 +1,18 @@
+import re
+from operator import add
+from functools import reduce
+
+# Define the pattern
+pattern = r"mul\((\d+),(\d+)\)"
+
+with open("3_input.txt", "r") as file:
+    text = file.read()
+
+# Find all matches
+matches = re.findall(pattern, text)
+
+# Convert matches to tuples of integers
+result = [(int(x), int(y)) for x, y in matches]
+
+print(reduce(add, map((lambda x: int(x[0]) * int(x[1])), matches)))
+
