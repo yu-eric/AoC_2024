d420cb2e2cb6c61d5e950715eb702543b810978c
yu-eric <ejyu99@gmail.com>
2024-12-06 23:49:44 +0000

    Finish first half

--- 06/guard_gallivant.py
+++ 06/guard_gallivant.py
@@ -1,16 +1,26 @@
+
 def patrol_path(grid):
     unique_positions = set()
-    positions = [[0, 1], [1, 0], [0, -1],  [-1, 0]]
+    positions = [[-1, 0], [0, 1], [1, 0],  [0, -1]]
     direction = positions[0]
     position = [39, grid[39].index("^")] # our starting point
 
     while 0 <= position[0] < len(grid[0]) and 0 <= position[1] < len(grid):
         unique_positions.add(tuple(position))
-        if grid[position[0] + direction[0]][position[1] + direction[1]] != "#":
+        try:
+            next_space = grid[position[0] + direction[0]][position[1] + direction[1]]
+        except IndexError:
+            return len(unique_positions)
+
+        if next_space != "#":
             position[0] += direction[0]
             position[1] += direction[1]
+
         else:
-            direction = (positions[positions.index(direction) + 1] if direction != [-1, 0] else positions[0])
+            while next_space == "#":
+                direction = (positions[positions.index(direction) + 1] if direction != [0, -1] else positions[0])
+                next_space = grid[position[0] + direction[0]][position[1] + direction[1]]
+
             position[0] += direction[0]
             position[1] += direction[1]
 
